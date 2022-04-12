b8e3515c302fe75c80f88645e8644705469d91ef
Kevin Loney <kevin.loney@brainsinjars.com>
2022-04-12 09:48:33 -0600

    Refactored Response.Unmarshal to no longer rely on mapstructure and to respect the json.Unmarshaler interface (#12)

---
+++ executor_test.go
@@ -0,0 +1,51 @@
+package dqlx
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/dgraph-io/dgo/v200/protos/api"
+
+	"github.com/stretchr/testify/require"
+)
+
+func UnmarshalTestHelper(name string, in interface{}, out interface{}) func(t *testing.T) {
+	return func(t *testing.T) {
+		var err error
+		r := Response{
+			Raw:         &api.Response{},
+			dataKeyPath: name,
+		}
+
+		if r.dataKeyPath == "" {
+			r.Raw.Json, err = json.Marshal(in)
+		} else {
+			r.Raw.Json, err = json.Marshal(map[string]interface{}{name: in})
+		}
+		require.NoError(t, err)
+
+		err = r.Unmarshal(out)
+		require.NoError(t, err)
+
+		require.Equal(t, in, out)
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	t.Helper()
+
+	// Test cases to ensure the DQL schema types are properly unmarshalled
+	// https://dgraph.io/docs/query-language/schema/#scalar-types
+	t.Run("dateTime", func(t *testing.T) {
+		t.Helper()
+
+		var out map[string]time.Time
+		in := map[string]time.Time{
+			"now": time.Now().UTC(),
+		}
+
+		t.Run("unnamed", UnmarshalTestHelper("", &in, &out))
+		t.Run("named", UnmarshalTestHelper("rootQuery", &in, &out))
+	})
+}
